abf9bf9ce4f187d67610a1c66f5c033d1562d669
nicocadq <nmachado.camejo@gmail.com>
2021-09-08 19:39:58 -0300

    add mocked response for tickets

--- index.ts
+++ index.ts
@@ -10,6 +10,29 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const tickets = [
+  {
+  "ticketId" : "xk3d03",
+  "subject" : "Hola, mi item esta roto",
+  "date" : "2020-03-01 01:30:00Z",
+  "from" : "Joaquin Perez",
+  "status" : "SOLVED",
+  "body": "Mi producto llego roto, no funciona como debería"
+  },
+  {
+  "ticketId" : "xx35",
+  "subject" : "Hola, mi item no llego",
+  "date" : "2020-03-02 05:30:00Z",
+  "from" : "Romina Esperanto",
+  "status" : "NEW",
+  "body": "Mi producto no llegó, cuando debería llegar?"
+  }
+];
+
+app.get('/tickets', (_request, response) => {
+  response.status(200).json(tickets);
+});
+
 app.get('/', (_request, response) => {
   response.send('Hello Tickets API');
 });
